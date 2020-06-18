6ceb7a30afd3a68d3bf520c292f24b27c34b5f02
samorodkin <samorodkin@gmail.com>
2020-06-18 12:01:58 +0300

    Added default wallet

--- scripts/get_balance.sh
+++ scripts/get_balance.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-/home/dmitry/net.ton.dev/tonos-cli/target/release/tonos-cli account $1 | grep balance | awk '{ print $2/1000000000 }'
+. ./env.sh
+
+if [ "$1" == "" ]
+then
+  addr=$( cat $KEYS_DIR/$(hostname -s).addr )
+else
+  addr=$1
+fi
+
+$TONOS_CLI_SRC_DIR/target/release/tonos-cli account $addr | grep balance | awk '{ print $2/1000000000 }'
+
